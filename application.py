fc44031358b70e679f476a4390d9c67946abbc0d
AndrewDongminYoo <ydm2790@gmail.com>
2021-10-20 06:13:21 +0900

    app.route

---
+++ application.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import render_template
+from flask.views import View, MethodView
+
+application = Flask(__name__, static_folder='static', template_folder='templates')
+
+
+@application.route('/', methods=["GET"])
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@application.errorhandler(404)
+def page_not_found(error):
+    return '404', 404
+
+
+@application.errorhandler(403)
+def permission_denied(error):
+    return '403', 403
+
+
+@application.route('/users/<str:username>')
+def show_users(username):
+    return username
+
+
+if __name__ == '__main__':
+    application.run()
